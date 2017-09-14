0871d099a69afc15ac8e8d0d14b08bb88805c83d
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-09-14 12:05:07 +0530

    add: gitignore, attributes fix: deps for deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,7 +33,7 @@ build() {
     echo "> build: start"
     # go get -v -u github.com/golang/dep/cmd/dep || true
     # dep ensure || true
-    go get -d -v -u || true
+    go get -d -u ./... || true
     go build -o "${build_target}"
     echo "> build: done"
 }
